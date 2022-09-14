5e4782f9458080b952e39d15806757b41a8c1f29
tazbirristy <tazbirristyweb@gmail.com>
2022-09-15 02:33:40 +0600

    convert-time-to-math-round

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ const gameOver = () => {
   // show result
   resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+    <p>You took: <span class="bold">${Math.round(timeTaken)}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
@@ -136,5 +136,5 @@ setInterval(() => {
   const timeSpent = (currentTime - startTime) / 1000;
 
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${Math.round(startTime) ? Math.round(timeSpent) : 0} seconds`;
 }, 1000);
